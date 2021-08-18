e068cc9daca1fe0ed30e2fac0ef3b50cfaf3d1e4
llolo121221 <salvarezb1212@gmail.com>
2021-08-18 11:21:51 -0500

    Cambio Unico

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,2 @@
 drop table if exists clientes;
-create table clientes(id long primary key auto_increment,nombre varchar(30),identificacion varchar(12)); 
+create table clientes(id long primary key auto_increment,nombre varchar(30),identificacion varchar(12) unique); 
